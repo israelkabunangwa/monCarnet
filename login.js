37eebb3abe736a29e43b06228000ce476934182d
Israel Kab's <112182230+israelkabunangwa@users.noreply.github.com>
2024-11-25 16:57:09 +0200

    Add files via upload

---
+++ login.js
@@ -0,0 +1,159 @@
+const {useState}=React
+
+/*const usersDB=[
+    {username : 'user1', password : 'pass1'},
+    {username : 'user2', password : 'pass2'},
+];
+
+function Login(){
+    const [username,setUsername]=useState('')
+    const [password,setPassword]=useState('')
+    const [errorMessage, setErrorMessage]=useState('')
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        //const user=usersDB.find(u= u.username===username && u.password === password);
+
+        if(username==='israel' && password==="password"){
+            window.location.href="maPage.html";
+        }else{
+            setErrorMessage("nom ou mot de pass incorect")
+        }
+    }
+    return(
+        <div>
+            <h2>Formulaire de connexion</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Nom"
+                    value={username}
+                    onChange={(e)=>setUsername(e.target.value)}
+                    required
+                />
+
+            <input
+                    type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
+                    required
+                />
+
+                <button type="submit">se connecter</button>
+            </form>
+            {errorMessage && <p>{errorMessage}</p>}
+        </div>
+    )
+}
+
+ReactDOM.render(<Login/>,document.getElementById('root'))*/
+const usersDB=[]
+
+function App(){
+    const [isLogin,setIslogin]=useState(true)
+
+
+    return(<div>
+        {isLogin ? (<Login setIsLogin={setIslogin}/>):(<Signup setIsLogin={setIslogin}/>)}
+    </div>)
+}
+
+    function Signup({ setIsLogin }) {
+            const [username, setUsername] = React.useState('');
+            const [password, setPassword] = React.useState('');
+            const [errorMessage, setErrorMessage] = React.useState('');
+    
+            const handleSubmit = (e) => {
+                e.preventDefault();
+                const userExists = usersDB.find(u => u.username === username);
+    
+                if (userExists) {
+                    setErrorMessage("Ce nom d'utilisateur est déjà pris.");
+                } else {
+                    usersDB.push({ username, password });
+                    alert("Compte créé avec succès !");
+                    setIsLogin(true); // Redirige vers le formulaire de connexion
+                }
+            };
+    
+            return (
+                <div className="login">
+                    <div className="loginContainer">
+                      <h2>Créer un Compte</h2>
+                    <form onSubmit={handleSubmit}>
+                        <input 
+                            type="text" 
+                            placeholder="Nom d'utilisateur" 
+                            value={username} 
+                            onChange={(e) => setUsername(e.target.value)} 
+                            required 
+                        />
+                        <input 
+                            type="password" 
+                            placeholder="Mot de passe" 
+                            value={password} 
+                            onChange={(e) => setPassword(e.target.value)} 
+                            required 
+                        />
+                        <button type="submit" className="seConnecter">Créer un Compte</button>
+                    </form>
+                    {errorMessage && <p className="error">{errorMessage}</p>}
+                    <p className="pasDeCompte">Déjà un compte ? <span onClick={() => setIsLogin(true)} className="creerCompte">Se connecter</span></p>
+                    </div>
+           
+                </div>
+            );
+        }
+
+
+
+       function Login({ setIsLogin }) {
+                const [username, setUsername] = React.useState('');
+                const [password, setPassword] = React.useState('');
+                const [errorMessage, setErrorMessage] = React.useState('');
+        
+                const handleSubmit = (e) => {
+                    e.preventDefault();
+                    const user = usersDB.find(u => u.username === username && u.password === password);
+        
+                    if (user) {
+                        // Redirection vers maPage.html
+                        window.location.href = "monCarnet.html";
+                    } else {
+                        setErrorMessage("Nom d'utilisateur ou mot de passe incorrect.");
+                    }
+                };
+        
+                return (
+                    <div className="login">
+                        <div className="loginContainer">
+                         <h2>Formulaire de Connexion</h2>
+                        <form onSubmit={handleSubmit}>
+                            <input 
+                                type="text" 
+                                placeholder="Nom d'utilisateur" 
+                                value={username} 
+                                onChange={(e) => setUsername(e.target.value)} 
+                                required 
+                            />
+                            <input 
+                                type="password" 
+                                placeholder="Mot de passe" 
+                                value={password} 
+                                onChange={(e) => setPassword(e.target.value)} 
+                                required 
+                            />
+                            <button type="submit" className="seConnecter">Se connecter</button>
+                        </form>
+                        {errorMessage && <p className="error">{errorMessage}</p>}
+                        <p className="pasDeCompte">Pas encore de compte ? <span onClick={() => setIsLogin(false)} className="creerCompte">Créer un Compte</span></p>
+                        </div>
+           
+                    </div>
+                );
+            }
+
+    ReactDOM.render(<App/>,document.getElementById('root'))
+
+    
---
+++ monCarnet.js
@@ -0,0 +1,132 @@
+const {useState,useEffect}=React
+
+function App(){
+
+    const [users,setUsers]=useState([])
+    const [first_name, setFirstname] =useState('');
+    const [last_name, setLastname] =useState('');
+    const [email, setEmail] =useState('');
+    const [editingIndex, setEditingIndex] =useState(null);
+
+    useEffect(() => {
+        fetch('https://reqres.in/api/users') 
+          .then(response => response.json())
+          .then(data => setUsers(data.data))
+       
+        
+      }, []);
+
+
+      const addUser = () => {
+        setUsers([...users, {id: Date.now(), first_name: first_name, last_name: last_name, email : email}]);
+        setFirstname('');
+        setLastname('');
+        setEmail('');
+       };
+
+       const handleDeleteUser = (index) => {
+        const updatedUsers = users.filter((_, i) => i !== index);
+         setUsers(updatedUsers);
+         };
+
+         const editUser = (index) => {
+            setFirstname(users[index].first_name);
+            setLastname(users[index].last_name);
+            setEmail(users[index].email);
+            setEditingIndex(index);
+        };
+
+        const handleAddOrUpdateUser = () => {
+             if (editingIndex !== null) {
+                        // Mise à jour de l'utilisateur existant
+             const updatedUsers = users.map((user, index) => {
+             if (index === editingIndex) {
+             return { first_name, last_name, email };
+             }
+             return user;
+             });
+             setUsers(updatedUsers);
+             setEditingIndex(null);
+             } else {
+                        // Ajout d'un nouvel utilisateur
+             setUsers([...users, { first_name, last_name, email  }]);
+             }
+             setFirstname('');
+             setLastname('');
+                    setEmail('');
+             };
+        
+      return(
+        <div className="admin">
+            <div class='title'>
+                <h2>Mes Contacts</h2>
+            </div>
+            <div class='adminContainer'>
+            <div class='userList'>
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="col">first_name</th>
+                        <th scope="col">last_name</th>
+                        <th scope="col">email</th>
+                    </tr>
+                </thead>
+             <tbody>
+                {users.map((user,index) => (
+                    <tr key={user.id}>
+                    
+                    <td>{user.first_name} </td>
+                    <td>
+                        {user.last_name}
+                    </td>
+                    <td>
+                        {user.email}
+                    </td>
+                    <td >
+                        <button onClick={() => editUser(index)}>Modifier</button>
+                    </td>
+                    <td >
+                    
+                        <button onClick={() => handleDeleteUser(index)}>Supprimer</button>
+                        
+                    </td>
+                    </tr>
+                    ))}
+            </tbody>
+            </table>
+            </div>
+         
+
+            <div class='form'>
+                <input 
+                    type="text" 
+                    placeholder="firstname"
+                    value={first_name} 
+                    onChange={(e) => setFirstname(e.target.value)} 
+                />
+                <input
+                    type="text" 
+                    placeholder="lastname"
+                    value={last_name} 
+                    onChange={(e) => setLastname(e.target.value)}   
+                />
+                <input 
+                    type="email" 
+                    placeholder="email"
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)}  
+                />
+               {editingIndex === null ? (
+                <button onClick={addUser} className="adminButton">Ajouter Utilisateur</button>
+                ) : (
+                <button onClick={handleAddOrUpdateUser} className="adminButton">Mettre à Jour Utilisateur</button>
+                )}
+            </div>
+            </div>
+           
+        </div>
+      )
+
+}
+
+ReactDOM.render(<App />,document.getElementById('root'))
